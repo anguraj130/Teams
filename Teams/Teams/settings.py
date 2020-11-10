d07f0eb6dfdc9ca869b3b1caf83bff809657ee37
ELCOT <anguraj130@gmai.com>
2020-11-10 12:26:39 +0530

    change2

--- Teams/Teams/settings.py
+++ Teams/Teams/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'Teams.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Teams',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
