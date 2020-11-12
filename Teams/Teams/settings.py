c8466b8b00bcac8ae8bada963df4c5a294876b80
ELCOT <anguraj130@gmai.com>
2020-11-12 21:08:44 +0530

    change 3

--- Teams/Teams/settings.py
+++ Teams/Teams/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bofa.apps.BofaConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
